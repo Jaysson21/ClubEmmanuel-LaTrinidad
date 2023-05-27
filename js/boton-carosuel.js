cacfb3d39df69116631e5eecd44a3495dbc4c124
Jaysson21 <jeysonfley@gmail.com>
2023-05-27 13:25:46 -0600

    Detalles pequeños 11

--- js/boton-carosuel.js
+++ js/boton-carosuel.js
@@ -1,73 +1,73 @@
-const carousel = document.querySelector(".carousel");
-const firstImg = carousel.querySelectorAll("img")[0];
-const arrowIcons = document.querySelectorAll(".wrapper i");
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
-const mostrarOcultarIconos = () => {
-    // Mostrar u ocultar el ícono anterior/siguiente según el valor del desplazamiento actual del carrusel
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // Obtener el ancho máximo desplazable
+const showHideIcons = () => {
+    // Mostrar u ocultar el icono previo/siguiente según el valor de desplazamiento izquierdo del carrusel
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // obteniendo el ancho máximo desplazable
     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // Obtener el ancho de la primera imagen y agregar un margen de 14
-        // Si el ícono clicado es el de la izquierda, reducir el valor del ancho del carrusel al desplazamiento a la izquierda; de lo contrario, sumarlo
+        let firstImgWidth = firstImg.clientWidth + 14; // obteniendo el ancho de la primera imagen y agregando 14 de valor de margen
+        // si el icono clicado es el izquierdo, se resta el valor del ancho desde el desplazamiento izquierdo del carrusel; de lo contrario, se suma
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => mostrarOcultarIconos(), 60); // Llamar a mostrarOcultarIconos después de 60 ms
+        setTimeout(() => showHideIcons(), 60); // llamando a showHideIcons después de 60ms
     });
 });
 
 const autoSlide = () => {
-    // Si no hay más imágenes por desplazar, se sale de la función
-    if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    // si no hay imágenes restantes para desplazarse, se retorna desde aquí
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
-    positionDiff = Math.abs(positionDiff); // Hacer que el valor de positionDiff sea positivo
+    positionDiff = Math.abs(positionDiff); // convirtiendo el valor de positionDiff a positivo
     let firstImgWidth = firstImg.clientWidth + 14;
-    // Obtener el valor de diferencia que se debe sumar o restar al desplazamiento del carrusel para centrar la imagen en el medio
+    // obteniendo el valor de diferencia que se debe agregar o restar desde el desplazamiento izquierdo del carrusel para centrar la imagen del medio
     let valDifference = firstImgWidth - positionDiff;
 
-    if (carousel.scrollLeft > prevScrollLeft) { // Si el usuario está desplazándose hacia la derecha
+    if(carousel.scrollLeft > prevScrollLeft) { // si el usuario se desplaza hacia la derecha
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
-    // Si el usuario está desplazándose hacia la izquierda
+    // si el usuario se desplaza hacia la izquierda
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
 
 const dragStart = (e) => {
-    // Actualizar los valores de las variables globales al inicio del arrastre (evento mousedown)
+    // actualizando el valor de las variables globales en el evento de clic del ratón
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
-    // Desplazar las imágenes/carrusel hacia la izquierda según la posición del puntero del ratón
-    if (!isDragStart) return;
+    // desplazando las imágenes/carrusel hacia la izquierda según la posición del ratón
+    if(!isDragStart) return;
     e.preventDefault();
     isDragging = true;
     carousel.classList.add("dragging");
     positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
-    mostrarOcultarIconos();
+    showHideIcons();
 }
 
 const dragStop = () => {
     isDragStart = false;
     carousel.classList.remove("dragging");
 
-    if (!isDragging) return;
+    if(!isDragging) return;
     isDragging = false;
     autoSlide();
 }
 
 carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
+//carousel.addEventListener("touchstart", dragStart);
 
-// Eliminar los eventos táctiles
-carousel.removeEventListener("touchstart", dragStart);
-carousel.removeEventListener("touchmove", dragging);
-carousel.removeEventListener("touchend", dragStop);
+document.addEventListener("mousemove", dragging);
+//carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+//carousel.addEventListener("touchend", dragStop);
